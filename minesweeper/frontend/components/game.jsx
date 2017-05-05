baba59c844626c3cd0f1d4b996bb85d045f293f2
Chris Cankov <cankov.chris@gmail.com>
2017-05-05 16:47:41 -0700

    Build Basic Game Display

---
+++ minesweeper/frontend/components/game.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Minesweeper from '../minesweeper';
+import Board from './board';
+
+class Game extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      board: new Minesweeper.Board(10, 25)
+    };
+
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame() {
+
+  }
+
+  render() {
+    return (
+      <Board board={this.state.board} updateGame={this.updateGame} />
+    );
+  }
+}
+
+export default Game;
---
+++ minesweeper/frontend/components/tile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Tile extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <p>T</p>
+    );
+  }
+}
+
+export default Tile;
