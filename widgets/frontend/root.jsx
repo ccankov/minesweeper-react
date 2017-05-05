29d780b7689baf5d70a3727d3c7a2bf7f1a1de2e
Chris Cankov <cankov.chris@gmail.com>
2017-05-05 11:49:27 -0700

    Create Project Skeleton

---
+++ widgets/frontend/root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Root extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>you made it</h1>
+    );
+  }
+}
+
+export default Root;
