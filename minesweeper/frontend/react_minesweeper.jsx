2cc50d0414dbff3f822544b7849f948189936f13
Chris Cankov <cankov.chris@gmail.com>
2017-05-05 16:00:18 -0700

    initial commit

---
+++ minesweeper/frontend/react_minesweeper.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Minesweeper from './minesweeper';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let root = document.getElementById('root');
+  ReactDOM.render( <div></div> , root);
+});
